dadf40f6779d909153d5280a956c586de931d2b6
tomattyy <oskamattheus.dev@gmail.com>
2026-07-30 15:59:52 -0300

    feat: arquivos adicionados

---
+++ main.py
@@ -0,0 +1,8 @@
+from operacoes import inverter_texto, gritar_texto
+
+# Prepare-se para uma mudança de paradigma
+resultado = inverter_texto("preciso estudar")
+print(resultado) # Saída: "radutse osicerp". Uma verdadeira quebra de expectativas.
+
+print(gritar_texto("estou procrastinando de forma elegante"))
+# Saída: ESTOU PROCRASTINANDO DE FORMA ELEGANTE!!!
